8c46c914f6d9c6c613441525292291c1c18f6f3f
Benyam Ephrem <ephremdsg@gmail.com>
2018-03-09 01:35:47 -0500

    Routes added for new wallet and settings components. Home dashboard component will be left intact to preserve file paths but eventually will be moved into the app/components folder later once everything is in place.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminComponent} from './layout/admin/admin.component';
+import {WalletComponent} from './components/wallet/wallet.component';
+import {SettingsComponent} from './components/settings/settings.component';
 
 const routes: Routes = [
   {
@@ -10,6 +12,14 @@ const routes: Routes = [
       {
         path: '',
         loadChildren: './theme/dashboard/default/default.module#DefaultModule'
+      },
+      {
+        path: 'wallet',
+        component: WalletComponent
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
 import {MenuItems} from './shared/menu-items/menu-items';
 import {BreadcrumbsComponent} from './layout/admin/breadcrumbs/breadcrumbs.component';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    WalletComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
