2eaf554abf2144c22f6b817c34ac34290e644da3
Benyam Ephrem <ephremdsg@gmail.com>
2018-03-09 03:21:31 -0500

    Hooked up main dashboard component with dynamic variables that will be manipulated by fetch function that will run on init. Currently wiring more and writing placeholder functions to extract info from DOM when user start, stops, or deletes a shard. More wiring to be done for wallet and settings.

--- src/app/theme/dashboard/default/default.component.ts
+++ src/app/theme/dashboard/default/default.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulati
 import {animate, style, transition, trigger} from '@angular/animations';
 
 import {NotificationsService} from 'angular2-notifications';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-default',
@@ -28,15 +29,64 @@ export class DefaultComponent implements OnInit, AfterViewInit {
   public feedbackData: any;
   public feedbackOption: any;
 
-    options: any = {
-      position: ['bottom', 'right'],
-    };
+  options: any = {
+    position: ['bottom', 'right'],
+  };
 
-    constructor(private servicePNotify: NotificationsService) {
+  // View variables that will be dynamically updated
+  shardsBeingMined: number;
+  totalShardsAvaliable: number;
+  totalBlocksFound: number;
+  totalBlocksImported: number;
 
-    }
+  // Statues
+  totalActive: number;
+  totalPaused: number;
+  totalFailed: number;
+
+  // Table information on held chains and what is being worked on
+  chainEntries:ChainEntry[];
+
+  constructor(private servicePNotify: NotificationsService, private dataService: DataService) {
+    this.shardsBeingMined = 13;
+    this.totalShardsAvaliable = 150;
+    this.totalBlocksFound = 23;
+    this.totalBlocksImported = 100;
+
+    this.chainEntries = [
+      {
+        type: 'mainchain',
+        id: 'main',
+        height: 403,
+        lastUpdated: '02-31-2018',
+        status: 'Paused'
+      },
+      {
+        type: 'shard',
+        id: '0x23af69fa526bbf12372e',
+        height: 14,
+        lastUpdated: '02-24-2018',
+        status: 'Active'
+      },
+      {
+        type: 'shard',
+        id: '0xfa526bbf123af69fa23',
+        height: 24,
+        lastUpdated: '03-01-2018',
+        status: 'Active'
+      }
+    ]
+
+    this.totalActive = 2;
+    this.totalPaused = 1;
+    this.totalFailed = 0;
+  }
 
   ngOnInit() {
+    // Fetch data to populate component with using data service
+    // this.dataService.getChainData().subscribe((data) => {
+    //   console.log(data);
+    // })
   }
 
   ngAfterViewInit() {
@@ -90,4 +140,13 @@ function f() {
     elements: {point: {radius: 4, borderWidth: 12}},
     layout: {padding: {left: 0, right: 0, top: 0, bottom: 0}}
   };
+}
+
+// Interface defining the each table row of chain information
+interface ChainEntry{
+    type:string, // 'shard' or 'mainchain' (corresponds to png image names)
+    id:string, // id of the chain. If it is the mainchain id is 'main'. If it is a shard it will look like this '0x23af69fa526bbf12372e'
+    height:number, // height of the blockchain
+    lastUpdated: string, // date this chain entry was last updated
+    status: string// 'active', 'paused', or 'failed'
 }
--- src/app/theme/dashboard/default/default.module.ts
+++ src/app/theme/dashboard/default/default.module.ts
@@ -6,14 +6,19 @@ import {SharedModule} from '../../../shared/shared.module';
 import {ChartModule} from 'angular2-chartjs';
 import {SimpleNotificationsModule} from 'angular2-notifications';
 
+import { DataService } from '../../../services/data.service';
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   imports: [
     CommonModule,
     DefaultRoutingModule,
     SharedModule,
     ChartModule,
+    HttpModule,
     SimpleNotificationsModule.forRoot()
   ],
+  providers: [DataService],
   declarations: [DefaultComponent],
   bootstrap: [DefaultComponent]
 })
